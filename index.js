8f08ba70264818f73fd33ba7cba64246263e9eed
rahem sorour <sorourrahem17@gmail.com>
2023-07-21 04:41:36 +0100

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name){
+    return `Hi, my name is ${name}`
+
+}
+
+function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is [${name}] and I am learning to program in [${language}] .`);
+  }
+
+  function introductionWithLanguageOptional(name, language='javascript') {
+    console.log(`Hi, my name is [${name}] and I am learning to program in [${language}] .`);
+  }  
+
+ 
